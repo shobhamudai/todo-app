124b9d6d19fafee5ea4322bcce269f26397f16b4
shobha mudai <Shobha Mudai>
2025-10-24 19:46:32 -0700

    Fixed lombok issue

--- backend/src/main/java/com/example/model/Todo.java
+++ backend/src/main/java/com/example/model/Todo.java
@@ -1,37 +1,14 @@
 package com.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
     private String id;
     private String task;
     private boolean completed;
-
-    public Todo(String id, String task, boolean completed) {
-        this.id = id;
-        this.task = task;
-        this.completed = completed;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTask() {
-        return task;
-    }
-
-    public void setTask(String task) {
-        this.task = task;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
 }
